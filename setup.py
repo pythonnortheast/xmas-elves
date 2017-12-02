4ed131893abba68c1a92b34355ca5c8f88857ca0
Scott Walton <scott.walton@mypebble.co.uk>
2017-12-02 18:14:12 +0000

    Added more requirements + deployment

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
+from os import path
 from setuptools import setup
+import m2r
 
 with open('requirements.txt') as f:
     requirements = [l.split(' ')[0] for l in f.readlines()]
 
+readme_file = path.join(path.dirname(path.abspath(__file__)),
+                        'README.md')
+try:
+    DESCRIPTION = m2r.parse_from_file(readme_file)
+except (IOError, FileNotFoundError):
+    DESCRIPTION = ''
 
 setup(name='pyne-xmas-elves',
-      version='0.2',
+      version='0.3',
       description='The Great Elf Game adapted for Python North East - client & server',
       url='http://github.com/pythonnortheast/xmas-elves',
       author='Scott Walton',
       author_email='scott@pythonnortheast.com',
       license='MIT',
+      long_description=DESCRIPTION,
       packages=['server.elves'],
       python_requires='>=3.5',
       install_requires=requirements,
