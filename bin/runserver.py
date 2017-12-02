b61497b8255716354fcf8e18f9a1512e57625218
Scott Walton <scott.walton@mypebble.co.uk>
2017-12-02 17:34:37 +0000

    Added basic setup.py process so we can pip install the server

---
+++ bin/runserver.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import subprocess
+import sys
+
+
+def run():
+    if not getattr(sys, 'base_prefix', ''):
+        print('This must be run from within an virtualenv')
+        exit(1)
+
+    print('Starting server...')
+    subprocess.run(['python', 'server/manage.py', 'runserver'])
+
+
+if __name__ == '__main__':
+    run()
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = [l.split(' ')[0] for l in f.readlines()]
+
+
+setup(name='pyne-xmas-elves',
+      version='0.1',
+      description='The Great Elf Game adapted for Python North East - client & server',
+      url='http://github.com/pythonnortheast/xmas-elves',
+      author='Scott Walton',
+      author_email='scott@pythonnortheast.com',
+      license='MIT',
+      packages=['server.elves'],
+      install_requires=requirements,
+      scripts=[
+          'bin/runserver.py',
+      ],
+      zip_safe=False)
